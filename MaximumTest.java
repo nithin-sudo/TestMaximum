8e6279d15f42d5a36279b745503d641a6a301a82
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-13 17:06:51 +0530

    Added:Generic method to find Maximum variable

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,6 +1,6 @@
 package com.generic.maxtest;
 public class MaximumTest<T extends Comparable<T>> {
-    T x,y,z;
+    T x,y,z,a;
 
     /**
      * defining constructor
@@ -8,13 +8,14 @@ public class MaximumTest<T extends Comparable<T>> {
      * @param y
      * @param z
      */
-    public MaximumTest(T x, T y, T z) {
+    public MaximumTest(T x, T y, T z,T a) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.a = a;
     }
    public T maximum(){
-        return MaximumTest.maximum(x,y,z);
+        return MaximumTest.maximum(x,y,z,a);
     }
     /**
      * using generic method to find the maximum values
@@ -24,7 +25,7 @@ public class MaximumTest<T extends Comparable<T>> {
      * @param <T>
      * @return
      */
-    public static <T extends Comparable>T maximum(T x,T y,T z){
+    public static <T extends Comparable>T maximum(T x,T y,T z,T a){
         T max=x;
         if(y.compareTo(max)>0){
             max=y;
@@ -32,11 +33,14 @@ public class MaximumTest<T extends Comparable<T>> {
         if (z.compareTo(max)>0){
             max=z;
         }
-        printMax(x,y,z,max);
+        if (a.compareTo(max)>0){
+            max=a;
+        }
+        printMax(x,y,z,a,max);
         return max;
     }
-    public static <E>void printMax(E x,E y,E z, E max) {
-        System.out.println("Maximum of "+x+" ,"+y+" ,"+z+"  is "+max);
+    public static <E>void printMax(E x,E y,E z, E a,E max) {
+        System.out.println("Maximum of "+x+" ,"+y+" ,"+z+","+a+ " is "+max);
     }
 
     /**
@@ -44,11 +48,11 @@ public class MaximumTest<T extends Comparable<T>> {
      * @param args
      */
     public static void main(String[] args) {
-        Integer aI=3,bI=4,cI=6;
-        Float eD=1.2222f,fD=2.3123f,gD=1.1234f;
-        String pS="dell",qS="asus",rD="linux";
-        new MaximumTest(aI,bI,cI).maximum();
-        new MaximumTest(eD,fD,gD).maximum();
-        new MaximumTest(pS,qS,rD).maximum();
+        Integer aI=3,bI=4,cI=6,dI=3;
+        Float eD=1.2222f,fD=2.3123f,gD=1.1234f,hD=1.211f;
+        String pS="dell",qS="asus",rS="linux",sS="hp";
+        new MaximumTest(aI,bI,cI,dI).maximum();
+        new MaximumTest(eD,fD,gD,hD).maximum();
+        new MaximumTest(pS,qS,rS,sS).maximum();
     }
 }
